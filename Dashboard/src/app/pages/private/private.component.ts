d09de586c120fd2ba569eac84d52260be1920dcc
Lars Eric Kroll <32699019+lekroll@users.noreply.github.com>
2022-01-14 17:22:43 +0100

    add private route

--- Dashboard/src/app/pages/private/private.component.ts
+++ Dashboard/src/app/pages/private/private.component.ts
@@ -9,139 +9,157 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class PrivateComponent implements OnInit {
 
-  
-  constructor(private api:ApiService, private auth:AuthService) { }
 
-  actions =  ['Regionen beschreiben','Analysen']
-  intend ="";
-  metadata:any;
-  sortdata:any;
-  geojson_available:any;
-  levelid:string;
-  level:string;
-  levelvalues:any;
-  currentlevel:string;
-  currentregion:string;
-  mapdata:any;
-  regiondata:any;
-  overalldata:any;
-  andata:any;
-  outcomes:any;
-  outcome:string;
-  determinants:any;
-  determinant:string;
-  controls:any;
-  regressiondata:any;
+  constructor(private api: ApiService, private auth: AuthService) { }
+  tsquery={};
+  tsqueryresult={};
+  settings: any;
+  duration:number;
+  levelvalues = [
+    'Gesamt',
+    'Baden-Württemberg',
+    'Bayern',
+    'Berlin',
+    'Brandenburg',
+    'Bremen',
+    'Hamburg',
+    'Hessen',
+    'Mecklenburg-Vorpommern',
+    'Niedersachsen',
+    'Nordrhein-Westfalen',
+    'Rheinland-Pfalz',
+    'Saarland',
+    'Sachsen',
+    'Sachsen-Anhalt',
+    'Schleswig-Holstein',
+    'Thüringen'];
+  outcomes = [
+    'Anteil Assessments', 
+    'Anzahl Assessments',
+    'Anzahl Assessments je 100 Tsd. Einw.',
+    'Anzahl Beschwerden pro Assessment',
+    'Anzahl Fragen je Assessment',
+    'Mittlere Dauer je Assessment',
+    'Mittlere Dauer Disposition',
+    'Anteil Assessment an Disposition',
+    'Häufigste Beschwerden',
+    'Häufigste Hauptbeschwerden']
+
+  diffmerkmale = [
+    'KV', 
+    'Alter', 
+    'Geschlecht', 
+    'Wochentag', 
+    'Empfehlung Dringlichkeit', 
+    'Empfehlung Ort', 
+    'Entscheidung Dringlichkeit', 
+    'Entscheidung Ort',
+    'Hauptbeschwerde',
+    'Beschwerde'
+];
+
+diffvars = {
+  'KV':'levelid', 
+  'Alter':['ALTER_id','ALTER_text'], 
+  'Geschlecht':['Geschlecht'], 
+  'Wochentag':[], 
+  'Empfehlung Dringlichkeit':['TTTsmed_id','TTTsmed_text'], 
+  'Empfehlung Ort':['POCsmed_id','POCsmed_text'], 
+  'Entscheidung Dringlichkeit':['TTTdispo_id','TTTdispo_text'], 
+  'Entscheidung Ort':['POCdispo_id','POCdispo_text'], 
+  'Hauptbeschwerde':['Hauptbeschwerde'],
+  'Beschwerde':['Beschwerden_gesamt']
+}
 
   ngOnInit(): void {
-    this.controls=[];
-    this.intend = this.actions[0];
-    this.metadata = this.api.getmetadata("metadata");
-    this.sortdata = this.api.getmetadata("sortdata");
-    this.geojson_available = this.api.getmetadata("geodata");
-    setInterval(()=>{this.auth.getRefreshToken()},60*1000*5)
-    this.updatesortinfo();
-    if (!this.levelid){
-      setTimeout(()=>{this.updatesortinfo();},750);
+    this.settings = { 'analyzeall': true, 'level': 'KV', 'levelid': 'Gesamt', 'diffmerkmale': [] };
+  }
+
+
+
+  updatequery(key, value) {
+    //console.log("Settings changed");
+    if (key != "__change" && key != "diffmerkmale") { this.settings[key] = value };
+    if (key == "levelid" && value == "Gesamt") { this.settings['analyzeall'] = true; };
+    if (key == 'diffmerkmale') {
+      if (this.settings['diffmerkmale'].includes(value)) {
+        this.settings['diffmerkmale'] = this.settings['diffmerkmale'].filter(function (item) { return item !== value });
+      }
+      else {
+      if (!this.settings['diffmerkmale'].includes(value) && this.settings['diffmerkmale'].length < 2) {
+        this.settings['diffmerkmale'].push(value);
+      };  
+    };    
     }
-    setTimeout(()=>{this.getregionandata();},750);
+  if (this.checkparams()){
+    this.timeseriesquery();
+  }  
   }
 
-  ngOnDestroy(){
-    this.regiondata=null;
-    this.andata=null;
-    this.regressiondata=null;
-    this.mapdata=null;
-    this.overalldata=null;
+  checkparams(){
+    let res = true;    
+    this.duration = 0;
+    if (this.settings['start'] && this.settings['end']){
+      this.duration=(this.settings['end']-this.settings['start'])/1000/60/60/24;
+    };
+    if (this.duration==0 || this.duration>90){
+      res = false;
+    };  
+    if (!this.settings['outcome']){
+      res = false;
     }
-
-
-  updatesortinfo(){
-    this.levelid=this.api.filterArray(this.metadata,"type","levelid")[0]['varname'];
-    this.level = this.api.filterArray(this.metadata, "type", "level")[0]["varname"];
-    this.levelid=this.api.filterArray(this.metadata,"type","levelid")[0]['varname'];
-    this.outcomes=this.api.filterArray(this.metadata,"topic","outcomes");
-    this.determinants=this.api.filterArray(this.metadata,"topic","demography");
-    this.levelvalues = this.api.filterArray(this.sortdata, "varname", this.level)[0]["values"].filter(x => this.geojson_available.includes(x));;
-    this.currentlevel=this.levelvalues[0];
-    this.newlevel(this.currentlevel);
-    this.currentregion="";
-  }
-  newlevel(level){  
-    this.currentlevel=level;
-    this.api.getTypeRequest('get_geodata/?client_id=' + this.api.REST_API_SERVER_CLIENTID + '&levelname=' + this.currentlevel).subscribe(
-      data => {
-        this.mapdata = data;
-      });
+    return res;
   }
 
-  changeregion(event){
-    this.currentregion=event;
-    this.getregiondata();
-  }
-
-  getregiondata(){
-    // Region
-    let query = {
-      "client_id": this.api.REST_API_SERVER_CLIENTID,
-      "groupinfo": {}
+  timeseriesquery(){
+    let tzoffset = (new Date()).getTimezoneOffset() * 60000;
+    let start ="";
+    if (this.settings['start']){start= (new Date(this.settings['start'] - tzoffset)).toISOString();};
+    let end = "";
+    if (this.settings['end']){end= (new Date(this.settings['end'] - tzoffset)).toISOString();};
+    this.tsqueryresult={};
+    this.tsquery= {
+        "startdate": start.slice(0,10),
+        "stopdate": end.slice(0,10),
+        "timeframe": "none",
+        "outcome": NaN,
+        "filterlist": [
+          {'level':'KV'}                      
+        ],
+        "subgroups": []     
+    };   
+    if (this.settings['levelid']!="Gesamt"){this.tsquery['filterlist'].push({'levelid':this.settings['levelid']})};
+    for (let item of this.settings['diffmerkmale']){
+      this.tsquery['subgroups']=this.tsquery['subgroups'].concat(this.diffvars[item]);
     };
-    // Overall
-    let refquery = {
-      "client_id": this.api.REST_API_SERVER_CLIENTID,
-      "groupinfo": {}
+    if (this.settings['diffmerkmale'].includes('Wochentag')){
+      this.tsquery['timeframe']='weekday';
     };
-    for (let item of this.metadata){
-      if (['ordering','subgroups'].indexOf(item.topic)>=0){
-        refquery["groupinfo"][item.varname]=item.allforlevel;
-      }       
-    }
-    query["groupinfo"][this.level] = this.currentlevel;
-    query["groupinfo"][this.levelid] = this.currentregion;
-    
-    this.api.postTypeRequest('get_data/', query).subscribe(data => {
-      this.regiondata=data['data'][0];
-      this.api.postTypeRequest('get_data/', refquery).subscribe(data => {
-        this.overalldata=data['data'][0];
-      },
-      error => {});
-    },
-    error => {});
-    
-    
+    if ('Anzahl Beschwerden pro Assessment'==this.settings['outcome']){
+      this.tsquery['outcome']="Anzahl_Beschwerden";
+    };
+    if ('Anzahl Fragen je Assessment'==this.settings['outcome']){
+      this.tsquery['outcome']="Anzahl_Fragen";
+    };
+    if ('Mittlere Dauer je Assessment'==this.settings['outcome']){
+      this.tsquery['outcome']="DAUERsmed";
+    };
+    if ('Mittlere Dauer Disposition'==this.settings['outcome']){
+      this.tsquery['outcome']="DAUERdispo";
+    };
+    if ('Anteil Assessment an Disposition'==this.settings['outcome']){
+      // later 2 Queries need to be done, one for DAUERsmed and one DAUERdispo
+      // console.log("Not implemented yet!");
+    };
+    if ('Häufigste Beschwerden'==this.settings['outcome']){
+      this.tsquery['outcome']="Beschwerden_gesamt";
+    };
+    if ('Häufigste Hauptbeschwerden'==this.settings['outcome']){
+      this.tsquery['outcome']="Hauptbeschwerde";
+    };
+    this.api.postTypeRequest('analytics/timeseries/',this.tsquery).subscribe(data => {this.tsqueryresult=data['result'];});
   }
 
-  removeoutcome(item){
-    this.controls = this.controls.filter(x=>x!==item);
-  }
 
-  getregionandata(){
-    if (this.outcome && this.determinant){
-      let query = {
-        "client_id": this.api.REST_API_SERVER_CLIENTID,
-        "groupinfo": {}
-      };
-      query["groupinfo"][this.level] = this.currentlevel;
-      query["showfields"]=[this.outcome,this.determinant];
-      this.andata=null;
-      this.api.postTypeRequest('get_data/', query).subscribe(data => 
-        {let res = data;this.andata=res['data'];});       
-     }
-  }
-  getregresults(){
-    if (this.outcome && this.determinant){
-    let anquery ={
-      "client_id": this.api.REST_API_SERVER_CLIENTID,
-      "groupinfo": {},
-       "theoutcome": this.outcome,
-       "maininterest": this.determinant,
-       "controls": this.controls
-     };
-     anquery["groupinfo"][this.level] = this.currentlevel;
-     this.regressiondata=null;
-     this.api.postTypeRequest('analytics/regression/', anquery).subscribe(data => 
-       {let res = data;this.regressiondata=res;        
-      }); 
-    };
-  }
 }
+
