8984f89c7a2c93d3c52a5126b55c2be2be78ace0
Wormann <nwormann@zi.de>
2024-02-21 15:06:14 +0100

    copied most static html over

--- Dashboard/src/app/components/page-not-found/page-not-found.component.ts
+++ Dashboard/src/app/components/page-not-found/page-not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-not-found.component.scss']
 })
 export class PageNotFoundComponent {
+
+  constructor(private router: Router) {}
+
+  goToRoot() {
+    this.router.navigate(['/']); 
+  }
 
 }
