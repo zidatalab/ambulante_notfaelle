48cb069bdc6dcd4d14720a2689b5ee1cb917061a
Wormann <nwormann@zi.de>
2024-03-25 14:39:25 +0100

    implemented new profile change password handling

--- Dashboard/src/app/pages/profile/profile.component.ts
+++ Dashboard/src/app/pages/profile/profile.component.ts
@@ -1,59 +1,119 @@
-import { Component, OnInit } from '@angular/core'; 
-import { Router } from '@angular/router'; 
-import { AuthService } from 'src/app/services/auth.service'; 
-import {ApiService} from '../../services/api.service' 
- 
-@Component({ 
-  selector: 'app-profile', 
-  templateUrl: './profile.component.html', 
-  styleUrls: ['./profile.component.scss'] 
-}) 
-export class ProfileComponent implements OnInit { 
- 
-  constructor( 
-    private api : ApiService, 
-    private _auth: AuthService, 
-    private router:Router
-  ) { } 
- 
-  userdetails:any;
-  pwdchange:any;
-  action:string;
-  pwdrequest:any;
-  deleterequest:any;
-  deleteresult:string;
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
+import { AuthService } from 'src/app/services/auth.service';
 
-  ngOnInit(): void { 
-    this.pwdrequest={'oldpwd':'','newpwd':''}
-    this.deleterequest={'pwd':''}
-    this.pwdchange= {'progress':false,'ok':false};
-    this.userdetails = this._auth.getUserDetails();
-    
-  } 
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss']
+})
+export class ProfileComponent implements OnInit {
 
-  chpwd(){
-    this.pwdchange['progress'] = true;
-    this.api.changeuserpwd(this.userdetails.email,this.pwdrequest.newpwd,this.pwdrequest.oldpwd).subscribe(data=>{
-    this.pwdchange['ok'] = true;
-    this.pwdchange['progress'] = false;
-    this.pwdchange['done'] = true;
-    setTimeout(()=>{
-      this.pwdchange= {'progress':false,'ok':false};this.action=''},1500);
-      this.pwdrequest={'oldpwd':'','newpwd':''};    
-  },
-  error => {
-    this.pwdchange['done'] = true;
-    this.pwdchange['progress'] = false;})
+  constructor(
+    private auth: AuthService,
+    private api: ApiService,
+    private router: Router
+  ) { }
+
+  user: any = {}
+  isChangePassword: boolean = false
+  passwordChangeRequest: any = { 'oldpwd': '', 'newpwd': '', 'newpwd_confirm': '' }
+  deleteAccountRequest: any = { 'pwd': '' }
+  error: any = {}
+  isPasswordMatchError: boolean = false
+  passwordMatchError: string = 'Die Passwörter stimmen nicht überein.'
+  isPasswordValid: boolean = false
+  passwordValidateError: string = 'Das Passwort muss mindestens 8 Zeichen lang sein, eine Zahl, einen Großbuchstaben enthalten'
+
+  ngOnInit(): void {
+    this.user = this.getUser()
   }
 
-  deleteaccount(){
-    this.api.deleteuser(this.userdetails.email,this.deleterequest.pwd).subscribe(
-      data => {
-        //this.deleteresult="success";
-        //localStorage.clear();
-        //setTimeout(()=>{this.router.navigate(['/'])},3000);        
-      },
-      error => {this.deleteresult="error";}
-    )
+  async getUser() {
+    this.user = await this.auth.getUserDetails()
   }
+
+  openChangePassword() {
+    this.isChangePassword = true
+  }
+
+  closeChangePassword() {
+    this.passwordChangeRequest = { 'oldpwd': '', 'newpwd': '', 'newpwd_confirm': '' }
+    this.isChangePassword = false
+  }
+
+  changePassword() {
+    if (!this.isPasswordMatchError) {
+      this.api.changeuserpwd(this.user.email, this.passwordChangeRequest.newpwd, this.passwordChangeRequest.oldpwd).subscribe(data => {
+        this.isChangePassword = false
+        this.auth.logout()
+        window.location.reload();
+        this.router.navigate(['/'])
+        alert('Passwort wurde geändert. Bitte loggen Sie sich mit ihrem neuen Passwort ein.')
+      }, error => {
+        this.isChangePassword = false
+        this.error = error.error.error
+        alert(this.error)
+      })
+    }
+  }
+
+  checkPasswordMatch() {
+    this.isPasswordMatchError = this.passwordChangeRequest.newpwd !== this.passwordChangeRequest.newpwd_confirm
+  }
+
+  validatePassword() {
+    // Password must be atleast 8 characters long
+    if (this.passwordChangeRequest.newpwd.length < 8) {
+      this.isPasswordValid = false
+      return
+    }
+
+    // Password must contain at least one uppercase letter
+    if (!/[A-Z]/.test(this.passwordChangeRequest.newpwd)) {
+      this.isPasswordValid = false
+      return
+    }
+
+    // Password must contain at least one number
+    if (!/\d/.test(this.passwordChangeRequest.newpwd)) {
+      this.isPasswordValid = false
+      return
+    }
+
+    // Password must not contain whitespace
+    if (/\s/.test(this.passwordChangeRequest.newpwd)) {
+      this.isPasswordValid = false
+      return
+    }
+
+    this.isPasswordValid = true;
+    return
+  }
+
+  showPassword(id: string, iconId: string) {
+    const passwordField = document.getElementById(id) as HTMLInputElement;
+    const icon = document.getElementById(iconId) as HTMLInputElement;
+
+    if (passwordField.type === "password") {
+      icon.innerText = 'visibility_off'
+      passwordField.type = "text";
+    } else {
+      icon.innerText = 'visibility'
+      passwordField.type = "password";
+    }
+  }
+
+  deleteAccount() {
+    // this.api.deleteUser(this.user.email, this.deleteAccountRequest.pwd).subscribe(data => {
+    //   this.auth.logout()
+    //   this.router.navigate(['/'])
+    // }, error => {
+    //   this.error = error.error.error
+    //   alert(this.error)
+    // })
+  }
+
 }
+
